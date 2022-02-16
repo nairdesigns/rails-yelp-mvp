dfeb16dd28d44d9779419a8121366a77dea9adae
nairdesigns <nairbdg@gmail.com>
2022-02-16 12:16:51 +0900

    restaurant model done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, exclusion: { in: %w[neptunian] }
+  has_many :reviews, dependent: :destroy
 end
--- db/migrate/20220216025217_create_restaurants.rb
+++ db/migrate/20220216025217_create_restaurants.rb
@@ -2,8 +2,8 @@ class CreateRestaurants < ActiveRecord::Migration[6.1]
   def change
     create_table :restaurants do |t|
       t.string :name
-      t.string, :address
-      t.string, :phone_number
+      t.string :address
+      t.string :phone_number
       t.string :category
 
       t.timestamps
