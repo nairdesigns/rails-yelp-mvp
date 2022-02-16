8b68898f65cd05532a11cae3339a9134df86300a
nairdesigns <nairbdg@gmail.com>
2022-02-16 15:46:36 +0900

    everything passing

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,22 +1,23 @@
 class ReviewsController < ApplicationController
-  def index
-    @reviews = Review.all
-  end
-
-  def show
-    @review = Review.find(params[:id])
-  end
-
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
     @review = Review.new(review_params)
+
+    # we need `restaurant_id` to associate review with corresponding restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
     if @review.save
-      redirect_to review_path(@review)
+      redirect_to restaurant_path(@restaurant)
     else
-      render 'new.html.erb'
+      render :new
     end
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating, :restaurant_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :reviews
-  resources :restaurants # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: %i[new create]
+  end # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
