580ff786a843b26f8a9b6a0e5c71060d968a994d
nairdesigns <nairbdg@gmail.com>
2022-02-16 12:25:01 +0900

    review valudations done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,5 @@
 class Review < ApplicationRecord
+  validates :content, presence: true
+  validates :rating, inclusion: 0..5, presence: true, numericality: { only_integer: true }
+  validates :restaurant_id, presence: true
 end
